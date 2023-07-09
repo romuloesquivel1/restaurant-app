2e21962e6da9a36ed66fd40e310c175fb86d1726
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-07-09 10:23:48 -0600

    refactor: remove javascript unnecessary code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-
-document.addEventListener("DOMContentLoaded", () => {
-  const menuButton = document.querySelector(".menu-button");
-  const menuList = document.querySelector(".menu-list");
-
-  menuButton.addEventListener("click", () => {
-    menuList.classList.toggle("hidden")
-  })
-})
+import "@hotwired/turbo-rails";
+import "./controllers";
