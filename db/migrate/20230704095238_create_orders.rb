87ed6d004168d7dd213f4fbb82c4366b263096e2
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-07-09 10:28:20 -0600

    fix: update default value for order date field

--- db/migrate/20230704095238_create_orders.rb
+++ db/migrate/20230704095238_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
-      t.date :date, default: DateTime.now
+      t.date :date, default: -> { 'CURRENT_TIMESTAMP' }
       t.float :sub_total, default: 0
       t.float :taxes, default: 10
       t.float :total, default: 0
