16b06d42ad046ed2367b147ab0579132000d6dfd
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-07-10 23:52:58 -0600

    refactor: fix restaurant validations and phone column name

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,11 @@
 class Restaurant < ApplicationRecord
   has_many :dishes, dependent: :destroy
-  # has_many :orders
+  has_many :orders, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 2, maximum: 35 }
-  validates :description, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 500 }
   validates :address, presence: true, length: { minimum: 2, maximum: 150 }
-  validates :phone, presence: true, numericality: true, length: { minumum: 10, maximum: 10 }
+  validates :phone_number, presence: true, numericality: true, length: { minumum: 10, maximum: 10 }
   validates :logo, presence: true
 
   has_one_attached :logo
