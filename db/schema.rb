6417f44f9c8c4cdd24c9ecfe1bad34a66c7a503b
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-07-11 00:00:17 -0600

    refactor: change db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_203116) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_095349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,33 +47,38 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_203116) do
     t.string "name"
     t.string "description"
     t.float "price"
+    t.bigint "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "restaurant_id", null: false
     t.index ["restaurant_id"], name: "index_dishes_on_restaurant_id"
   end
 
   create_table "items", force: :cascade do |t|
     t.float "price"
+    t.bigint "order_id", null: false
+    t.bigint "dish_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["dish_id"], name: "index_items_on_dish_id"
+    t.index ["order_id"], name: "index_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "date"
-    t.float "sub_total"
-    t.float "taxes"
-    t.float "total"
+    t.date "date", default: -> { "CURRENT_TIMESTAMP" }
+    t.float "sub_total", default: 0.0
+    t.integer "taxes", default: 10
+    t.float "total", default: 0.0
+    t.bigint "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_orders_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.text "description"
+    t.string "description"
     t.string "address"
-    t.string "phone"
-    t.string "logo"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -81,4 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_203116) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "dishes", "restaurants"
+  add_foreign_key "items", "dishes"
+  add_foreign_key "items", "orders"
+  add_foreign_key "orders", "restaurants"
 end
