c4ae2eadd404d10af6deb9b06887eaa3385d1cd3
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-07-11 23:43:31 -0600

    fix: favicon path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Serve static assets
-  config.serve_static_assets = true
 end
