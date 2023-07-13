0bbacafdd05a5d0ffd248abe16ab9f81337cc1b8
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-07-13 16:26:46 -0600

    changes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,22 @@ class ItemsController < ApplicationController
 
     @item = Item.new
   end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @order = Order.find(params[:order_id])
+    @item = @order.items.build(item_params)
+    dish_id = params[:item][:dish_id]
+    @selected_dish = @restaurant.dishes.find_by(id: dish_id)
+
+    if @item.save
+      redirect_to restaurant_order_path(@restaurant, @order)
+    else
+      render 'new'
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:price, :order_id, :dish_id)
+  end
 end
