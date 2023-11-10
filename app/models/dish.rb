a0061bc9b9e64168c44806ed0f12b91030390d71
Romulo Esquivel <romulo.esquivel1@outlook.com>
2023-11-10 13:29:12 +0000

    add assets for demo

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,8 +4,8 @@ class Dish < ApplicationRecord
   has_one_attached :photo
 
   validates :dish_group, presence: true, length: { minimum: 2, maximum: 25 }
-  validates :name, presence: true, length: { minimum: 2, maximum: 35 }
-  validates :description, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 135 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 355 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :photo, presence: true
 end
